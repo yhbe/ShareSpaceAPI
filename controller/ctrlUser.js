0e62055d39c023ab3d3a28b2006ebfa1df5c436d
SHEF <Shefqetmush@gmail.com>
2023-03-17 16:55:32 -0400

    send token on login

--- controller/ctrlUser.js
+++ controller/ctrlUser.js
@@ -1,48 +1,55 @@
 const User = require("../models/userBlog")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 module.exports = {
   post: async (req, res) => {
     try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
       const newUser = new User({
         username: req.body.username,
         fullname: req.body.fullname,
         emailaddress: req.body.emailaddress,
-        password: hashedPassword
+        password: hashedPassword,
       })
 
       await newUser.save()
       return res.status(201).json({ message: "User created", newUser });
     } catch (error) {
-      return res.status(404).json({message: error.message})
+      return res.status(404).json({ message: error.message })
     }
   },
-  verifyUser: async(req,res) => {
+  verifyUser: async (req, res) => {
     try {
-      const user = await User.findOne({username: req.body.username})
+      const user = await User.findOne({ username: req.body.username });
 
       if (!user) {
-        return res.status(404).json({message: "User  not found"})
+        return res.status(404).json({ message: "User  not found" });
       }
 
-      const isMatch = await bcrypt.compare(req.body.password, user.password)
+      const isMatch = await bcrypt.compare(req.body.password, user.password);
 
-      if (!isMatch){
-        return res.status(401).json({message: "Invalid credentials"})
+      if (!isMatch) {
+        return res.status(401).json({ message: "Invalid credentials" });
       }
 
       const userToSend = {
         username: user.username,
         fullname: user.fullname,
         emailaddress: user.emailaddress,
-        id: user._id
+        id: user._id,
       };
 
-      return res.status(200).json({message: "User Found", user: userToSend})
-    } catch (error){
-      return res.status(500).json({message: error.message})
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "3h",
+      });
+
+      return res
+        .status(200)
+        .json({ message: "User Found", user: userToSend,token });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
     }
-  }
+  },
 };
