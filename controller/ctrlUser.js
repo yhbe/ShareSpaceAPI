5099e07d56afdedc60efdd9e71cb2118fafea7f1
SHEF <Shefqetmush@gmail.com>
2023-03-19 17:07:36 -0400

    handle logout request

--- controller/ctrlUser.js
+++ controller/ctrlUser.js
@@ -99,4 +99,12 @@ module.exports = {
       return res.status(500).json({ message: error.message });
     }
   },
+  logOut: async(req,res) => {
+    try {
+      res.clearCookie("logInToken");
+      return res.status(200).json({ message: "User logged out successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
 };
--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.post("/", controller.post)
 router.post("/login", controller.verifyUser)
 router.get("/loginWithCookies", controller.tryLoginWithCookies)
+router.post("/logout", controller.logOut)
 
 module.exports = router
